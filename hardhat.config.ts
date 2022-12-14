4f87c36c0f51e7af71bc56a0c35861934627e6f6
louis <Louiis1997@hotmail.fr>
2022-12-14 14:42:33 +0100

    Update hardhat.config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config();
+
+const { API_URL, PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+  solidity: "0.8.9",
+  defaultNetwork: "polygon_mumbai",
+  networks: {
+    hardhat: {},
+    polygon_mumbai: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+}
 
 export default config;
