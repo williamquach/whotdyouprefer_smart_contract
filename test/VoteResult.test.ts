ddd05ffa60b7d19d94757ede3c3c9db0f7a67a84
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-12-29 12:28:21 +0100

    add tests for session results

--- test/VoteResult.test.ts
+++ test/VoteResult.test.ts
@@ -1,11 +1,8 @@
-import {Contract, ContractFactory} from "ethers";
+import {BigNumber, Contract, ContractFactory} from "ethers";
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 
 import {ethers} from "hardhat";
 import {expect} from 'chai';
-
-const chai = require('chai');
-chai.use(require('chai-shallow-deep-equal'));
 
 describe("VoteResult Contract", function() {
     let VoteResult: ContractFactory;
@@ -40,11 +37,126 @@ describe("VoteResult Contract", function() {
                 await voteResult.connect(addr4).createVote(0, [2, 1, 0, 3]);
                 await voteResult.connect(addr5).createVote(0, [3, 1, 0, 2]);
             });
-            it("Should return choice ids array for session 0", async function() {
-                expect(await voteResult.choicesForSession[0]).to.deep.equal([0, 1, 2, 3]);
+
+            it("Should return choice ids array for session 0", async function () {
+                expect(await voteResult.getChoiceBySessionId(0)).to.deep.equal([0, 1, 2, 3]);
             });
-            it("Should return a results table of the initial votes by choice", async function() {
-                expect(await voteResult.getInitializeResultArray(voteResult.choicesForSession[0])).to.equal([[2, 1, 2, 0],[1, 3, 0, 0],[1, 0, 2, 1],[1, 0, 0, 3]]);
+
+            it("Should return a results table of the initial votes by choice", async function () {
+                expect(await voteResult.getInitializeResultArray(await voteResult.getAllVotesBySessionId(0),await voteResult.getChoiceBySessionId(0))).to.deep.equal(
+                    [
+                        [
+                            BigNumber.from(2),
+                            BigNumber.from(1),
+                            BigNumber.from(2),
+                            BigNumber.from(0)
+                        ],
+                        [
+                            BigNumber.from(1),
+                            BigNumber.from(4),
+                            BigNumber.from(0),
+                            BigNumber.from(0)
+                        ],
+                        [
+                            BigNumber.from(1),
+                            BigNumber.from(0),
+                            BigNumber.from(3),
+                            BigNumber.from(1)
+                        ],
+                        [
+                            BigNumber.from(1),
+                            BigNumber.from(0),
+                            BigNumber.from(0),
+                            BigNumber.from(4)
+                        ]
+                    ]
+                );
+            });
+
+            it("Should return the loser of the first turn", async function () {
+                let result =  await voteResult.getInitializeResultArray(await voteResult.getAllVotesBySessionId(0),await voteResult.getChoiceBySessionId(0));
+                expect(await voteResult.getTheLoserFromTheRound(result, await voteResult.getChoiceBySessionId(0))).to.deep.equal(3);
+            });
+
+            describe("Should return votes redissolved",  function () {
+                let result: any;
+                let votes: any;
+                let choiceIds: any;
+
+                beforeEach(async function () {
+                    result =  await voteResult.getInitializeResultArray(await voteResult.getAllVotesBySessionId(0),await voteResult.getChoiceBySessionId(0));
+                    votes = await voteResult.getAllVotesBySessionId(0);
+                    choiceIds = await voteResult.getChoiceBySessionId(0);
+
+                });
+
+                it("Of the second round", async function () {
+                    expect(await voteResult.rediscoverTheVotes(3, result, votes, choiceIds, 2)).to.deep.equal(
+                        [
+                            [
+                                BigNumber.from(2),
+                                BigNumber.from(1),
+                                BigNumber.from(2),
+                                BigNumber.from(0)
+                            ],
+                            [
+                                BigNumber.from(2),
+                                BigNumber.from(4),
+                                BigNumber.from(0),
+                                BigNumber.from(0)
+                            ],
+                            [
+                                BigNumber.from(1),
+                                BigNumber.from(0),
+                                BigNumber.from(3),
+                                BigNumber.from(1)
+                            ],
+                            [
+                                BigNumber.from(1),
+                                BigNumber.from(0),
+                                BigNumber.from(0),
+                                BigNumber.from(4)
+                            ]
+                        ]
+                    );
+                });
+
+                it("Of the third round", async function () {
+                    result = await voteResult.rediscoverTheVotes(3, result, votes, choiceIds, 2)
+                    choiceIds = await voteResult.remove(choiceIds, 3);
+                    expect(await voteResult.rediscoverTheVotes(2, result, votes, choiceIds, 3)).to.deep.equal(
+                        [
+                            [
+                                BigNumber.from(3),
+                                BigNumber.from(1),
+                                BigNumber.from(2),
+                                BigNumber.from(0)
+                            ],
+                            [
+                                BigNumber.from(3),
+                                BigNumber.from(4),
+                                BigNumber.from(0),
+                                BigNumber.from(0)
+                            ],
+                            [
+                                BigNumber.from(1),
+                                BigNumber.from(0),
+                                BigNumber.from(3),
+                                BigNumber.from(1)
+                            ],
+                            [
+                                BigNumber.from(1),
+                                BigNumber.from(0),
+                                BigNumber.from(0),
+                                BigNumber.from(4)
+                            ]
+                        ]
+                    );
+                });
+            });
+
+            it("Should return the winner of the session", async function () {
+                expect(await voteResult.getWinnerBySessionId(0)).to.deep.equal(1);
             });
         });
     });
