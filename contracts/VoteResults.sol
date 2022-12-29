e9e54431d5b96eb7364b93fdd04e144e9e17b700
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-12-29 12:25:01 +0100

    refacto all functions for session results

--- contracts/VoteResults.sol
+++ contracts/VoteResults.sol
@@ -5,80 +5,104 @@ import "./VoteResults.sol";
 
 contract VoteResults is VoteFactory{
 
-    event GetResult(Result);
-
-    struct Result {
-        uint sessionId;
-        uint [][] result;
-        uint choiceIdWinner;
-    }
-
-    Result[] public results;
-
     function getWinnerBySessionId(uint _sessionId) external view returns(uint) {
         uint[] memory choiceIds = sessionToChoices[_sessionId];
         Vote[] memory votes = getAllVotesBySessionId(_sessionId);
-        //turn 1
-        uint[][] memory result = getInitializeResultArray(votes);
-        uint loserId = getTheLoserFromTheTurn(choiceIds, result);
-        //turn 2
-        uint turn = 2;
-        // distribuer les voix du perdant tour 2
-        result = rediscoverTheVotes(loserId, result, votes, turn);
+        //round 1
+        uint[4][4] memory result = getInitializeResultArray(votes, choiceIds);
+        uint loserId = getTheLoserFromTheRound(result, choiceIds);
+        //round 2
+        uint round = 2;
+        // rediscover the votes of the loser round 2
+        result = rediscoverTheVotes(loserId, result, votes, choiceIds, round);
         choiceIds = remove(choiceIds, loserId);
-        loserId = getTheLoserFromTheTurn(choiceIds, result);
-        //turn 3
-        turn++;
-        // distribuer les voix du perdant tour 3
-        result = rediscoverTheVotes(loserId, result, votes,  turn);
+        loserId = getTheLoserFromTheRound(result, choiceIds);
+        //round 3
+        round++;
+        // rediscover the votes of the loser round 3
+        result = rediscoverTheVotes(loserId, result, votes, choiceIds, round);
         choiceIds = remove(choiceIds, loserId);
-        loserId = getTheLoserFromTheTurn(choiceIds, result);
-        // last turn
+        loserId = getTheLoserFromTheRound(result, choiceIds);
+        // last round
         choiceIds = remove(choiceIds, loserId);
-        return choiceIds[0];
+        return getWinnerOfTheLastRound(choiceIds);
     }
 
-    function getInitializeResultArray(Vote[] memory _votes) public pure returns(uint[][] memory) {
-        uint[][] memory result;
-        for(uint i = 0; i < _votes.length; i++) {
+    function getInitializeResultArray(Vote[] memory _votes, uint[] memory _choiceIds) public pure returns(uint[4][4] memory) {
+        uint[4][4] memory result;
+        for(uint i = 0; i < _choiceIds.length+1; i++) {
             for(uint j = 0; j < _votes[i].choiceIds.length; j++) {
-                result[_votes[i].choiceIds[j]][j]++;
+                if ( _votes[i].choiceIds[j] == _choiceIds[0]) result[0][j]++;
+                else if ( _votes[i].choiceIds[j] == _choiceIds[1]) result[1][j]++;
+                else if ( _votes[i].choiceIds[j] == _choiceIds[2]) result[2][j]++;
+                else if ( _votes[i].choiceIds[j] == _choiceIds[3]) result[3][j]++;
             }
         }
         return result;
     }
 
-    function rediscoverTheVotes(uint _loserId, uint[][] memory _result, Vote[] memory _votes,  uint _turn) private pure returns(uint[][] memory) {
+    function rediscoverTheVotes(uint _loserId, uint[4][4] memory _result, Vote[] memory _votes,uint[] memory _choiceIds,  uint _round) public pure returns(uint[4][4] memory) {
         for(uint i = 0; i < _votes.length; i++){
-            if(_votes[i].choiceIds[0] == _loserId) {
-                if(_turn == 2 || _turn == 3) {
-                    _result[_votes[i].choiceIds[1]][0]++;
+            if(_votes[i].choiceIds[0] == _choiceIds[_loserId]) {
+                if(_round == 2 || _round == 3) {
+                    if (_choiceIds[0] == _votes[i].choiceIds[1]) _result[0][0]++;
+                    else if (_choiceIds[1] == _votes[i].choiceIds[1]) _result[1][0]++;
+                    else if (_choiceIds[2] == _votes[i].choiceIds[1]) _result[2][0]++;
+                    else if (_choiceIds[3] == _votes[i].choiceIds[1]) _result[3][0]++;
                 }
-                if(_turn == 3) {
-                    _result[_votes[i].choiceIds[2]][0]++;
+                if(_round == 3) {
+                    if (_choiceIds[0] == _votes[i].choiceIds[2]) _result[0][0]++;
+                    else if (_choiceIds[1] == _votes[i].choiceIds[2]) _result[1][0]++;
+                    else if (_choiceIds[2] == _votes[i].choiceIds[2]) _result[2][0]++;
+                    else if (_choiceIds[3] == _votes[i].choiceIds[2]) _result[3][0]++;
                 }
             }
         }
         return _result;
     }
 
-    function getTheLoserFromTheTurn(uint[] memory _choices, uint[][] memory _result) private pure returns(uint){
-        uint loserId = _choices[0];
-        for(uint i  = 1; i < _choices.length; i++) {
-            if (_result[_choices[i]][0] < _result[loserId][0]) {
-                loserId = _choices[i];
+    function getTheLoserFromTheRound(uint[4][4] memory _result, uint[] memory _choiceIds) public pure returns(uint){
+        uint loserId = 0;
+        for(uint i = 1; i < _result.length; i++) {
+            if(isRemoved(_choiceIds, i)){
+                continue;
+            } else if (_result[i][0] < _result[loserId][0]) {
+                loserId = i;
+            } else if (_result[i][0] == _result[loserId][0]) {
+                if (_result[i][1] < _result[loserId][1]){
+                    loserId = i;
+                } else if (_result[i][1] == _result[loserId][1]){
+                    if (_result[i][2] < _result[loserId][2]){
+                        loserId = i;
+                    } else if (_result[i][2] == _result[loserId][2]){
+                        if (_result[i][3] < _result[loserId][3]){
+                            loserId = i;
+                        }
+                    }
+                }
             }
         }
         return loserId;
     }
 
-    function remove(uint[] memory _array, uint choiceId) private pure returns(uint[] memory){
-        uint[] memory newArray;
-        _array[choiceId] = _array[_array.length - 1];
+    function isRemoved(uint[] memory _array, uint _choiceId) private pure returns(bool){
+        return _array[_choiceId] == 99999999;
+    }
 
-    for (uint i = 0; i < _array.length - 1; i++) {
-            _array[i] = newArray[i];
+    function remove(uint[] memory _array, uint _choiceId) public pure returns(uint[] memory){
+        uint[] memory newArray = new uint[](_array.length);
+        for (uint i = 0; i < _array.length; i++) {
+            if(i == _choiceId) newArray[i] = 99999999;
+            else newArray[i] = _array[i];
         }
         return newArray;
+    }
+
+    function getWinnerOfTheLastRound(uint[] memory _choiceIds) private pure returns(uint){
+        uint winner = 99999999;
+        for(uint i = 0; i < _choiceIds.length ; i++){
+            if (_choiceIds[i] != 99999999) return  i;
+        }
+        return winner;
     }
 }
